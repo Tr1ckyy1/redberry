1a08c728e98064610d2ef7b577a58db5d857fe45
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2023-12-19 08:12:32 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+      <div>s</div>
+    </>
+  );
+}
+
+export default App;
